71f1406f5b73a0febdfb7d0230eec2130441eed5
Puleeno Nguyen <puleeno@gmail.com>
2020-09-18 12:36:39 +0700

    Create closure for Jankx class

--- src/BlocksManager.php
+++ src/BlocksManager.php
@@ -1,10 +1,33 @@
 <?php
 namespace Jankx\Blocks;
 
+use Jankx;
+
 if (!class_exists(BlocksManager::class)) {
     class BlocksManager
     {
+        protected static $instance;
+
+        public static function getInstance()
+        {
+            if (is_null(static::$instance)) {
+                static::$instance = new static();
+            }
+            return static::$instance;
+        }
+
+        private function __construct()
+        {
+            /**
+             * Create block closure for Jankx class
+             */
+            $jankxInstance  = Jankx::getInstance();
+            $blocksInstance = &$this;
+            $jankx->blocks  = function () use ($blocksInstance) {
+                return $blocksInstance;
+            };
+        }
     }
 
-    new BlocksManager();
+    BlocksManager::getInstance();
 }
