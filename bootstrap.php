00ba60f2c87ec45c8bacdc19f906c65c81c51d9c
Puleeno Nguyen <puleeno@gmail.com>
2021-06-16 15:44:18 +0700

    Create a ads block on top

--- bootstrap.php
+++ bootstrap.php
@@ -1,6 +1,17 @@
 <?php
 use Jankx\Blocks\BlocksManager;
+use Elementor\Plugin as Elementor;
 
 if (class_exists(BlocksManager::class)) {
     BlocksManager::getInstance();
+}
+
+/**
+ * Create block positions
+ */
+add_action('jankx_template_before_header', 'jankx_block_top_ads');
+function jankx_block_top_ads()
+{
+    $elementor = Elementor::instance();
+    echo $elementor->frontend->get_builder_content_for_display( 25 );
 }
